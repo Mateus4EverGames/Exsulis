e1ee3b82f209ec61b39a33240b4597f9da1d02d3
Thiago José Fonseca de Oliveira <Thiago50570676@edu.df.senac.br>
2025-08-15 17:43:15 -0300

    movimentação

--- Assets/Codagem/PlayerMove.cs
+++ Assets/Codagem/PlayerMove.cs
@@ -5,6 +5,7 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private float moveInput;
+    private bool noChao = true;
     [SerializeField] private float velocidade;
     [SerializeField] private float forcaPulo;
     [SerializeField] private int vida;
@@ -12,13 +13,60 @@ public class PlayerMove : MonoBehaviour
 
     void Start()
     {
+        velocidade = 5f;
+        forcaPulo = 5f;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        moveInput = Input.GetAxis("Horizontal");
         
+           if (moveInput != 0f)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(moveInput), 1f, 1f);
+            }
+
+            if(noChao && Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+                noChao = false;
+                //anim.SetBool
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+           //     Ataque();
+                //anim.SetBool
+            }
     }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * velocidade, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Chao"))
+        {
+            noChao = true;
+        }
+    }
+
+    /*public void Ataque()
+    {
+        Collider2D[] inimigos = Physics2D.OverlapCircleAll(transform.position, 1f, LayerMask.GetMask("Inimigo"));
+        foreach (Collider2D inimigoCol in inimigos)
+        {
+            Inimigos inimigo = inimigoCol.GetComponent<Inimigos>();
+            if (inimigo != null)
+            {
+                inimigo.ReceberDano(dano);
+            }
+        }
+
+    }*/
 }
