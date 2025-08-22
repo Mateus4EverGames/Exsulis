4a1fb3d57524f88ab853d95f5c54b6cc70c4a0b6
Thiago José Fonseca de Oliveira <Thiago50570676@edu.df.senac.br>
2025-08-22 17:31:51 -0300

    starting tilemap

--- Assets/Codagem/CameraSegue.cs
+++ Assets/Codagem/CameraSegue.cs
@@ -4,10 +4,12 @@ public class CameraSegue : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
 
     void Start()
     {
         player  = FindFirstObjectByType<PlayerMove>().transform;
+        offset = transform.position - player.position;
     }
 
 
@@ -18,6 +20,6 @@ public class CameraSegue : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = player.position + offset;
     }
 }
